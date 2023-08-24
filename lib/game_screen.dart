2ca341bcb2638aa4eed618955797b191d76fc316
badhon <badhonx102@google.com>
2023-08-24 17:15:38 +0600

    Showing winning result after play

--- lib/game_screen.dart
+++ lib/game_screen.dart
@@ -9,6 +9,7 @@ class GameScreen extends StatefulWidget {
 }
 
 class _GameScreenState extends State<GameScreen> {
+  String playerDeclaration = '';
   bool oTurn = true;
   List<String> displayXO = [
     '',
@@ -71,7 +72,13 @@ class _GameScreenState extends State<GameScreen> {
           ),
           Expanded(
             flex: 1,
-            child: const Text(" time"),
+            child: Text(
+              playerDeclaration,
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold),
+            ),
           ),
         ],
       ),
@@ -82,10 +89,84 @@ class _GameScreenState extends State<GameScreen> {
     setState(() {
       if (oTurn && displayXO[index] == '') {
         displayXO[index] = 'O';
-      } else {
+      } else if (!oTurn && displayXO[index] == '') {
         displayXO[index] = 'X';
       }
       oTurn = !oTurn;
+      _checkWinner();
     });
+  }
+
+  _checkWinner() {
+    //1st row
+    if (displayXO[0] == displayXO[1] &&
+        displayXO[1] == displayXO[2] &&
+        displayXO[0] != '') {
+      setState(() {
+        playerDeclaration = "Player ${displayXO[0]} Wins";
+      });
+    }
+    //2nd row
+    if (displayXO[3] == displayXO[4] &&
+        displayXO[4] == displayXO[5] &&
+        displayXO[3] != '') {
+      setState(() {
+        playerDeclaration = "Player ${displayXO[3]} Wins";
+      });
+    }
+
+    //3rd row
+    if (displayXO[6] == displayXO[7] &&
+        displayXO[7] == displayXO[8] &&
+        displayXO[6] != '') {
+      setState(() {
+        playerDeclaration = "Player ${displayXO[6]} Wins";
+      });
+    }
+
+    //1st Column
+    if (displayXO[0] == displayXO[3] &&
+        displayXO[3] == displayXO[6] &&
+        displayXO[0] != '') {
+      setState(() {
+        playerDeclaration = "Player ${displayXO[0]} Wins";
+      });
+    }
+
+    //2nd Column
+    if (displayXO[1] == displayXO[4] &&
+        displayXO[4] == displayXO[7] &&
+        displayXO[1] != '') {
+      setState(() {
+        playerDeclaration = "Player ${displayXO[0]} Wins";
+      });
+    }
+
+    //3rd Column
+    if (displayXO[2] == displayXO[5] &&
+        displayXO[5] == displayXO[8] &&
+        displayXO[2] != '') {
+      setState(() {
+        playerDeclaration = "Player ${displayXO[2]} Wins";
+      });
+    }
+
+    //1st creoss
+    if (displayXO[0] == displayXO[4] &&
+        displayXO[4] == displayXO[8] &&
+        displayXO[0] != '') {
+      setState(() {
+        playerDeclaration = "Player ${displayXO[0]} Wins";
+      });
+    }
+
+    //2nd cross
+    if (displayXO[2] == displayXO[4] &&
+        displayXO[4] == displayXO[6] &&
+        displayXO[2] != '') {
+      setState(() {
+        playerDeclaration = "Player ${displayXO[2]} Wins";
+      });
+    }
   }
 }
